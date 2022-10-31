4a6563b3cd5697626053f8787604e51e4f84586e
ruka-chan <novops911@gmail.com>
2022-10-31 16:07:54 +0700

    ruka-chan

---
+++ parkir.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class parkir {
+    public static void main(String[] args) {
+
+    String Kendaraan;
+    int jam = 0;
+    int bayar = 0;
+    int totalbiaya = 0;
+    int parkir = 0;
+
+    Scanner myObj = new Scanner(System.in);
+
+    // Kendaraan parkir mobil
+
+    System.out.println("Nama Kendaraan anda : ");   
+    Kendaraan = myObj.nextLine(); 
+
+    System.out.println("Sudah berapa lama parkir? : ");
+    parkir  = myObj.nextInt();
+    
+    if(Kendaraan.equals("mobil")){
+        bayar = 5000; 
+        jam = 3000;
+    } else if(Kendaraan.equals("motor")) {
+        bayar = 3000;
+        jam = 2000;
+    } else if(Kendaraan.equals("truk")) {
+        bayar = 4000;
+        jam  = 4000;
+    } else {
+        System.out.println("kendaraan kmu tidak termasuk");
+    }
+
+    if(parkir <= 10) {
+        totalbiaya = bayar + ( parkir - 1 ) * jam ;
+    } else {
+        totalbiaya = bayar + 9 * jam; 
+    }
+
+    System.out.println("Total biaya adalah : "+ totalbiaya);
+    }
+}
